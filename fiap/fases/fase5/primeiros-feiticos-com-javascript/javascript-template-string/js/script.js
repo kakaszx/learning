b40e498d9e70a49f08dcd86b869bd292d0a6251b
kakaszx <kauaaurelio14@gmail.com>
2026-08-13 20:38:15 -0300

    aula-video-javascript-template-string

---
+++ fiap/fases/fase5/primeiros-feiticos-com-javascript/javascript-template-string/js/script.js
@@ -0,0 +1,15 @@
+let nome = "Superman";
+let poder = "Voar";
+let estaVoando = false;
+
+console.log(nome);
+console.log(poder);
+console.log(estaVoando);
+
+// Template
+let mensagem = `Nome: ${nome}, ele tem como poder ${poder}.
+Ele está voando? ${estaVoando}.
+
+Fim da template`;
+
+console.log(mensagem);
