bef7844900911741a6cc1f05e07d4b25f475f0fd
kakaszx <kauaaurelio14@gmail.com>
2026-08-13 07:08:28 -0300

    aula-video-javascript-tipagem-e-escopo-de-variaveis

---
+++ fiap/fases/fase5/primeiros-feiticos-com-javascript/javascript-tipagem-e-escopo-de-variaveis/js/script.js
@@ -0,0 +1,59 @@
+// Criando Variaveis
+
+let nome = "Clark Kent";
+let email = "clark@fiap.com.br";
+let idade = 39;
+let logado = true;
+
+// Exibindo no console
+console.log(nome);
+console.log(email);
+console.log(idade, logado);
+
+// Exibindo o tipo
+console.log(typeof nome);
+console.log(typeof email);
+console.log(typeof idade);
+console.log(typeof logado);
+console.log(typeof nome, nome);
+
+const DATA_NASCIMENTO = "02/11/1986;";
+
+console.log(DATA_NASCIMENTO);
+
+var novoNome = "Bruce Wayne";
+console.log(novoNome, typeof novoNome);
+
+var novoNome = "Logan";
+console.log(novoNome);
+
+console.log(poder);
+
+var poder = "voar";
+
+console.log(typeof nome, nome);
+console.log(typeof idade, idade);
+
+nome = 7894;
+idade = false;
+
+console.log(typeof nome, nome);
+console.log(typeof idade, idade);
+
+let valorInicial = true;
+console.log(valorInicial, typeof valorInicial);
+
+// Convertendo para String
+valorInicial = String(valorInicial);
+console.log(valorInicial, typeof valorInicial);
+
+// Convertendo para boolean
+valorInicial = Boolean(valorInicial);
+console.log(valorInicial, typeof valorInicial);
+
+// Convertendo para Number
+let numero = "123456";
+
+console.log(typeof numero, numero);
+numero = Number(numero);
+console.log(typeof numero, numero);
