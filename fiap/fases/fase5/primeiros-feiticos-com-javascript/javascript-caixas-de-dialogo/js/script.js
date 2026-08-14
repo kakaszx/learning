a07ccb0a310064dfefcf8e7f4e92a7cb56b3dd22
kakaszx <kauaaurelio14@gmail.com>
2026-08-14 06:35:24 -0300

    aula-video-javascript-caixas-de-dialogo

---
+++ fiap/fases/fase5/primeiros-feiticos-com-javascript/javascript-caixas-de-dialogo/js/script.js
@@ -0,0 +1,12 @@
+// Alert
+alert("Olá, sou um alerta!");
+
+// Prompt
+let mensagem = prompt("Entre com uma mensagem");
+
+// Exibir a mensagem
+console.log(`Mensagem digitada: ${mensagem}`);
+
+// Confirm
+let resposta = confirm("Deseja finalizar o aplicativo?");
+console.log(resposta);
