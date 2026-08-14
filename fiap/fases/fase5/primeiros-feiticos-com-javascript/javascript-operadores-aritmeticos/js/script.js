d135f2d0f31b2d1c537224cc7dbc72ccdf2b8074
kakaszx <kauaaurelio14@gmail.com>
2026-08-14 07:00:16 -0300

    aula-video-javascript-operadores-aritmeticos

---
+++ fiap/fases/fase5/primeiros-feiticos-com-javascript/javascript-operadores-aritmeticos/js/script.js
@@ -0,0 +1,26 @@
+let primeiroNumero = 50;
+let segundoNumero = 3;
+let resultado;
+
+console.log(`Primeiro Número = ${primeiroNumero}`);
+console.log(`Segundo Número = ${segundoNumero}`);
+
+// Adição
+resultado = primeiroNumero + segundoNumero;
+console.log(`${primeiroNumero} + ${segundoNumero} = ${resultado}`);
+
+// Subtração
+resultado = primeiroNumero - segundoNumero;
+console.log(`${primeiroNumero} - ${segundoNumero} = ${resultado}`);
+
+// Multiplicação
+resultado = primeiroNumero * segundoNumero;
+console.log(`${primeiroNumero} * ${segundoNumero} = ${resultado}`);
+
+// Divisão
+resultado = primeiroNumero / segundoNumero;
+console.log(`${primeiroNumero} / ${segundoNumero} = ${resultado}`);
+
+//Módulo
+resultado = primeiroNumero % segundoNumero;
+console.log(`${primeiroNumero} % ${segundoNumero} = ${resultado}`);
