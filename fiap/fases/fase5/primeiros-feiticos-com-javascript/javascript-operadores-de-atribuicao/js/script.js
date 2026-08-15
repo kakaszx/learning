a2edf4cd0d9981bfdf86fd9f772345d0ce3c3c8a
kakaszx <kauaaurelio14@gmail.com>
2026-08-15 13:33:21 -0300

    aula-video-javascript-operadores-de-atribuicao

---
+++ fiap/fases/fase5/primeiros-feiticos-com-javascript/javascript-operadores-de-atribuicao/js/script.js
@@ -0,0 +1,22 @@
+//criando variáveis
+let value = 50;
+console.log(`Valor = ${value}`);
+
+//aplicando operadores
+value += 10;
+console.log(`Novo valor = ${value}`);
+
+value -= 10;
+console.log(`Novo valor = ${value}`);
+
+value *= 10;
+console.log(`Novo valor = ${value}`);
+
+value /= 10;
+console.log(`Novo valor = ${value}`);
+
+value **= 10;
+console.log(`Novo valor = ${value}`);
+
+value %= 10;
+console.log(`Novo valor = ${value}`);
