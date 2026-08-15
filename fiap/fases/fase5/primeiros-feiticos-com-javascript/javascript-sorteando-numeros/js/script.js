3c192fad86f06d92767b5a50b9cb5cf76118941e
kakaszx <kauaaurelio14@gmail.com>
2026-08-15 15:53:56 -0300

    aula-video-javascript-sorteando-numeros

---
+++ fiap/fases/fase5/primeiros-feiticos-com-javascript/javascript-sorteando-numeros/js/script.js
@@ -0,0 +1,13 @@
+let numeroSorteado = Math.random();
+console.log(numeroSorteado);
+
+numeroSorteado *= 100;
+console.log(numeroSorteado);
+
+numeroSorteado = parseInt(numeroSorteado);
+console.log(numeroSorteado);
+
+console.log(`----------------------------`);
+
+let novoSorteio = Math.floor(Math.random() * 100);
+console.log(novoSorteio);
