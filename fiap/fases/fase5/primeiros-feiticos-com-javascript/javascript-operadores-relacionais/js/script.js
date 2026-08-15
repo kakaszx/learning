374a8c13ca3c761ba251d4b596deab9897ddb72c
kakaszx <kauaaurelio14@gmail.com>
2026-08-15 11:38:30 -0300

    aula-video-javascript-operadores-relacionais

---
+++ fiap/fases/fase5/primeiros-feiticos-com-javascript/javascript-operadores-relacionais/js/script.js
@@ -0,0 +1,38 @@
+let numero1 = 10;
+let numero2 = "20";
+let resultado;
+
+console.log(`Primeiro Número: ${numero1}`);
+console.log(`Segundo Número: ${numero2}`);
+
+// Igualdade Simples --> ==
+resultado = numero1 == numero2;
+console.log(`${numero1} == ${numero2} = ${resultado}`);
+
+// Desigualdade Simples --> !=
+resultado = numero1 != numero2;
+console.log(`${numero1} != ${numero2} = ${resultado}`);
+
+// Igualdade Estrita --> ===
+resultado = numero1 === numero2;
+console.log(`${numero1} === ${numero2} = ${resultado}`);
+
+// Desigualdade Estrita --> !==
+resultado = numero1 !== numero2;
+console.log(`${numero1} !== ${numero2} = ${resultado}`);
+
+// Maior --> >
+resultado = numero1 > numero2;
+console.log(`${numero1} > ${numero2} = ${resultado}`);
+
+// Maior ou Igual a --> >=
+resultado = numero1 >= numero2;
+console.log(`${numero1} >= ${numero2} = ${resultado}`);
+
+// Menor --> <
+resultado = numero1 < numero2;
+console.log(`${numero1} < ${numero2} = ${resultado}`);
+
+// Menor ou Igual a --> <=
+resultado = numero1 <= numero2;
+console.log(`${numero1} <= ${numero2} = ${resultado}`);
