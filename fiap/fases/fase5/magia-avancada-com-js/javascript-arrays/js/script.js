a2c753ae18d414d6d4d22b103aeb507b5edb03a4
kakaszx <kauaaurelio14@gmail.com>
2026-08-26 06:17:06 -0300

    aula-video-javascript-arrays

---
+++ fiap/fases/fase5/magia-avancada-com-js/javascript-arrays/js/script.js
@@ -0,0 +1,24 @@
+// Método construtor
+const arr = new Array(
+  "Televisão",
+  "Video Game",
+  "Celular",
+  "Notebook",
+  1256,
+  true,
+  789.45,
+  false,
+);
+
+console.log(arr);
+console.log(arr[1]);
+console.log(arr[3]);
+console.log(arr[5]);
+
+const pessoas = ["Eu", "Tu", "Ele", "Nós", "Vós", "Eles"];
+
+console.log(pessoas);
+console.table(pessoas);
+
+console.log(arr.length);
+console.log(`Quantidade de pessoas: ${pessoas.length}`);
